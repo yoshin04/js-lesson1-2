ed5ee26ba9b38caa972cd67e8dda42a5d73f87c3
yoshin04 <taigaby0411@gmail.com>
2020-11-22 13:22:00 +0900

    1-2修正

--- aleart.js
+++ aleart.js
@@ -1,12 +1,13 @@
 'use strict';
 let quiz = prompt('日本の首都は？');
-let correct = '正解です！';
-let incorrect = '不正解です！';
+const correct = '正解です！';
+const incorrect = '不正解です！';
 while(quiz) {
-  if(quiz == '東京') {
+  if(quiz === '東京') {
     alert(correct);
+    break;
   } else {
     alert(incorrect);
+    quiz = prompt('日本の首都は？');
   }
-  quiz = prompt('日本の首都は？');
 }
